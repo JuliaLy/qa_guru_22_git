fa27ab4c96092db9aa20ec94b711f626bb63b39b
Julia Lysakova <ulyaro@mail.ru>
2023-09-15 01:41:37 +0200

    hello message was changed

--- src/main/java/ru/ulyaro/Main.java
+++ src/main/java/ru/ulyaro/Main.java
@@ -2,6 +2,6 @@ package ru.ulyaro;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello qa.guru!");
     }
 }

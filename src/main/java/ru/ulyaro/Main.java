be5eb848b997b20ca656797e0d9db4f75b871e9e
JuliaLy <41026229+JuliaLy@users.noreply.github.com>
2023-09-15 01:51:55 +0200

    Update Main.java

--- src/main/java/ru/ulyaro/Main.java
+++ src/main/java/ru/ulyaro/Main.java
@@ -2,6 +2,6 @@ package ru.ulyaro;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru!");
+        System.out.println("Hello qa.guru!!!!!");
     }
 }
